7e38090de6ab09b88841a01a2afa85269ac16611
Felipe Guell <feliguells@gmail.com>
2023-03-02 22:52:18 -0300

    Función de cambiar el lenguaje a inglés

--- js/app.js
+++ js/app.js
@@ -13,3 +13,19 @@ inputs.forEach(input => {
 textArea.addEventListener("blur", (input) =>{
     valida(input.target);
 });
+
+
+
+ /* ----- Botón para cambiar a inglés ------- */
+
+ var check = document.querySelector(".english__language");
+ check.addEventListener('click', idioma);
+
+ function idioma(){
+    let id = check.checked;
+    if (id==true){
+        location.href="en/index.html"
+    }else{
+        location.href="../index.html"
+    }
+ }
